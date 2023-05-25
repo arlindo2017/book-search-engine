6ed4d1c9c32421f7c55404fd0cf1427eb8ff7760
Arlindo Pinto <perensen@gmail.com>
2023-05-24 21:43:59 -0400

    removed API code

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -18,12 +18,6 @@ const SavedBooks = () => {
     }
   }, [data]);
 
-  useEffect(() => {
-    if (data) {
-      setUserData(data.me);
-    }
-  }, [data]);
-
   // create function that accepts the book's mongo _id value as param and deletes the book from the database
   const handleDeleteBook = async (bookId) => {
     const token = Auth.loggedIn() ? Auth.getToken() : null;
